57c93c4bf86b301a229ad9e9c7c23ad15ba8f8b4
JC Coto <jc@fractalreef.com>
2021-08-04 16:56:12 -0600

    Update crypto price API to make markets and currencies pluggable

--- datatracker/v1/data/crypto_prices.py
+++ datatracker/v1/data/crypto_prices.py
@@ -2,14 +2,19 @@ import os
 
 import httpx
 
+from . import metrics
+
 PUBLIC_KEY = os.getenv("CRYPTOWATCH_PUBLIC_KEY")
 SECRET_KEY = os.getenv("CRYPTOWATCH_SECRET_KEY")
 
+MARKET = "bitfinex"
 
-async def get_current_price():
+
+async def get_current_price(metric):
     async with httpx.AsyncClient() as client:
+        metric_id = metrics.get_metric_id(metric)
         result = await client.get(
-            "https://api.cryptowat.ch/markets/coinbase-pro/btcusd/price",
+            f"https://api.cryptowat.ch/markets/{MARKET}/{metric_id}/price",
             headers={"X-CW-API-Key": PUBLIC_KEY},
         )
         return result.json()["result"]["price"]
