b03323749f1645da94070dc6cc03be068a877723
JC Coto <jc@fractalreef.com>
2021-08-04 16:55:40 -0600

    Add support for multiple metrics and general refactors to metrics

--- datatracker/v1/data/metrics.py
+++ datatracker/v1/data/metrics.py
@@ -1,4 +1,3 @@
-import decimal
 import datetime
 
 import aioredis
@@ -7,26 +6,49 @@ REDIS_URL = "redis://localhost/"
 MAIN_DB = 1
 TEST_DB = 2
 
-RANGE_KEY = "dt:datapoints"
-
 DEFAULT_HOURS = 24
 
+RANGE_KEY = "dt:data_points"
+RANK_KEY = "dt:ranks"
+
+METRICS = [
+    ("Bitcoin in USD", "btcusd"),
+    ("Bitcoin in EUR", "btceur"),
+]
 
 connection = aioredis.from_url(REDIS_URL, db=MAIN_DB)
+
+
+def get_all_metrics():
+    return METRICS
+
+
+def get_metric_id(metric):
+    return metric[1].strip().lower()
+
+
+def get_readable_name(metric):
+    return metric[0].strip()
 
 
 async def connect_for_testing_and_clear():
     print("Warning - Using test database in current process.")
     global connection
     connection = aioredis.from_url(REDIS_URL, db=TEST_DB)
-    await connection.delete(RANGE_KEY)
+    await connection.flushdb()
 
 
-async def get_latest(hours=DEFAULT_HOURS):
+def _get_key(metric):
+    metric_id = get_metric_id(metric)
+    return f"{RANGE_KEY}:{metric_id}"
+
+
+async def get_latest(metric, hours=DEFAULT_HOURS):
+    key = _get_key(metric)
     range_start = datetime.datetime.now() - datetime.timedelta(hours=hours)
 
     return await connection.zrangebyscore(
-        RANGE_KEY,
+        key,
         min=range_start.timestamp(),
         max="+inf",
         withscores=True,
@@ -34,17 +56,14 @@ async def get_latest(hours=DEFAULT_HOURS):
     )
 
 
-async def add_and_prune(data_point, hours=DEFAULT_HOURS):
+async def add_and_prune(metric, data_point, hours=DEFAULT_HOURS):
+    key = _get_key(metric)
     now = datetime.datetime.now()
     range_start = now - datetime.timedelta(hours=hours)
 
     async with connection.pipeline(transaction=True) as pipeline:
-        pipeline.zadd(RANGE_KEY, {data_point: now.timestamp()})
-        pipeline.zremrangebyscore(
-            RANGE_KEY, min="-inf", max=f"({range_start.timestamp()}"
-        )
+        pipeline.zadd(key, {data_point: now.timestamp()})
+        pipeline.zremrangebyscore(key, min="-inf", max=f"({range_start.timestamp()}")
         result = await pipeline.execute()
 
-    # all_items = await connection.zrange(RANGE_KEY, start=0, end=-1, withscores=True)
-    # breakpoint()
     return result
